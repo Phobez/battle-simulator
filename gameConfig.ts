b796067dfd0a3f1c0701b9f6bb6dd6347ef07c88
extremebip <anthonygt124@gmail.com>
2024-06-18 01:43:31 +0700

    Change board orientation config

--- gameConfig.ts
+++ gameConfig.ts
@@ -1,7 +1,9 @@
+import { Orientation } from "honeycomb-grid";
+
 export default {
-   maxFPS: 30,
    hex: {
       dimensions: 30,
       origin: "topLeft",
+      orientation: Orientation.FLAT,
    },
 } as const;
